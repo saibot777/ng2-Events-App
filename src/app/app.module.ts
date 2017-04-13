4d5095215eb9c6a9404f9ad0765e64d075306482
frenzy777 <stefantrajkovic777@gmail.com>
2017-04-13 17:12:05 +0200

    login, auth-service are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './nav/navbar.component';
 import {appRoutes} from "./routes";
 import {Error404Component} from "./errors/404.component";
+import {AuthService} from "./user/auth.service";
 
 
 @NgModule({
@@ -39,6 +40,7 @@ import {Error404Component} from "./errors/404.component";
     EventService,
     EventRouteActivator,
     EventListResolver,
+    AuthService,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
---
+++ src/app/user/user.model.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by stefan.trajkovic on 13.4.2017..
+ */
+export interface IUser {
+  id : number
+  firstName : string
+  lastName : string
+  userName : string
+}
