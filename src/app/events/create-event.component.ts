be4515ba6db2bfdd610d10ccb3e4a82e3e1336bd
frenzy777 <stefantrajkovic777@gmail.com>
2017-04-15 02:05:19 +0200

    added save event method

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {EventService} from "./shared/event.service";
 
 @Component({
   selector: 'app-create-event',
@@ -9,9 +10,15 @@ import {Router} from "@angular/router";
 export class CreateEventComponent implements OnInit {
   isDirty : boolean = true;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private eventService : EventService) { }
 
   ngOnInit() {
+  }
+
+  saveEvent(formValues) {
+    this.eventService.saveEvent(formValues)
+    this.isDirty = false;
+    this.router.navigate(['/events']);
   }
 
   cancel() {
