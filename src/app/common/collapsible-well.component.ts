cbd3d805d9a873090e567f49a21d9156f12fdae4
frenzy777 <stefantrajkovic777@gmail.com>
2017-04-17 14:30:06 +0200

    multiple slots projection

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -7,13 +7,14 @@ import {Component, Input} from '@angular/core';
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well hoverwell">
-      <h4 class="well-title">{{title}}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+      <h4>
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      <ng-content select="[well-body]" *ngIf="visible"></ng-content>
     </div>
   `
 })
 export class CollapsibleWellComponent {
-  @Input() title : string;
   visible : boolean = true;
 
   toggleContent() {
